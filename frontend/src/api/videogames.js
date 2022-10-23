d00d574304fad697362a566be5009cbb3b19aaf9
Daniel López <hyperdaniel579exe@gmail.com>
2022-10-23 03:30:29 -0500

    API requests

--- frontend/src/api/videogames.js
+++ frontend/src/api/videogames.js
@@ -1,7 +1,11 @@
-import axios from 'axios'
+import axios from "axios";
 
-export const getVideogamesRequests = async () => await axios.get("/videogames")
-export const getVideogameRequest = async id => await axios.get(`/videogames/${id}`)
-export const createVideogameRequest = async (videogame) => await axios.post("/videogames", videogame)
-export const updateVideogameRequest = async (id, newFields) => await axios.put(`/videogames/${id}`, newFields)
-export const deleteVideogameRequest = async id => await axios.delete(`/videogames/${id}`)
+export const getVideogamesRequests = async () => await axios.get("/videogames");
+export const getVideogameRequest = async (id) =>
+  await axios.get(`/videogames/${id}`);
+export const createVideogameRequest = async (videogame) =>
+  await axios.post("/videogames", videogame);
+export const updateVideogameRequest = async (id, newFields) =>
+  await axios.put(`/videogames/${id}`, newFields);
+export const deleteVideogameRequest = async (id) =>
+  await axios.delete(`/videogames/${id}`);
