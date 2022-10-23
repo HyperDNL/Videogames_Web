c2b3a36ecb4e090df448a33877f72e29d659f19a
Daniel López <hyperdaniel579exe@gmail.com>
2022-10-23 03:31:21 -0500

    Component card updated

--- frontend/src/components/VideogameCard.js
+++ frontend/src/components/VideogameCard.js
@@ -1,14 +1,21 @@
-import { MDBRipple } from 'mdb-react-ui-kit';
-import { useNavigate } from 'react-router-dom';
+import { MDBRipple } from "mdb-react-ui-kit";
+import { useNavigate } from "react-router-dom";
 
 export function VideogameCard({ videogame }) {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   return (
-    <div className="rounded-sm shadow-md hover:cursor-pointer">
-      <MDBRipple rippleTag='div' className='bg-image hover-overlay hover-zoom hover-shadow rounded'>
-        <img src={videogame.image_cover} className='w-100' />
-        <div onClick={() => navigate(`/viewVideogame/${videogame._id}`)} className='mask grid grid-cols-2 place-items-center' style={{ backgroundColor: 'rgba(30, 33, 38, 0.6)' }}></div>
+    <div className="cover-container">
+      <MDBRipple
+        rippleTag="div"
+        className="bg-image hover-overlay hover-zoom hover-shadow"
+      >
+        <img src={videogame.image_cover} className="w-100" />
+        <div
+          onClick={() => navigate(`/viewVideogame/${videogame._id}`)}
+          className="mask grid grid-cols-2 place-items-center"
+          style={{ backgroundColor: "rgba(30, 33, 38, 0.6)" }}
+        ></div>
       </MDBRipple>
     </div>
   );
