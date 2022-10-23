0fa593baee17403f3b63cd9dcf289f8dfb897ede
Daniel López <hyperdaniel579exe@gmail.com>
2022-10-23 03:31:04 -0500

    Context updated

--- frontend/src/context/videogameContext.js
+++ frontend/src/context/videogameContext.js
@@ -1,56 +1,69 @@
-import { useState, createContext, useContext, useEffect } from 'react'
-import { createVideogameRequest, getVideogamesRequests, deleteVideogameRequest, getVideogameRequest, updateVideogameRequest } from '../api/videogames'
+import { useState, createContext, useContext, useEffect } from "react";
+import {
+  createVideogameRequest,
+  getVideogamesRequests,
+  deleteVideogameRequest,
+  getVideogameRequest,
+  updateVideogameRequest,
+} from "../api/videogames";
 
-const videogamesContext = createContext()
+const videogamesContext = createContext();
 
 export const useVideogames = () => {
-  const context = useContext(videogamesContext)
-  return context
-}
+  const context = useContext(videogamesContext);
+  return context;
+};
 
 export const VideogameProvider = ({ children }) => {
-
-  const [videogames, setVideogames] = useState([])
+  const [videogames, setVideogames] = useState([]);
 
   const getVideogames = async () => {
-    const res = await getVideogamesRequests()
-    setVideogames(res.data)
-  }
+    const res = await getVideogamesRequests();
+    setVideogames(res.data);
+  };
 
   const getVideogame = async (id) => {
-    const res = await getVideogameRequest(id)
-    return res.data
-  }
+    const res = await getVideogameRequest(id);
+    return res.data;
+  };
 
   const createVideogame = async (videogame) => {
-    const res = await createVideogameRequest(videogame)
-    setVideogames([...videogames, res.data])
-  }
+    const res = await createVideogameRequest(videogame);
+    setVideogames([...videogames, res.data]);
+  };
 
   const updateVideogame = async (id, newFields) => {
-    const res = await updateVideogameRequest(id, newFields)
-    setVideogames(videogames.map((videogame) => (videogame._id === id ? res.data : videogame)))
-  }
+    const res = await updateVideogameRequest(id, newFields);
+    setVideogames(
+      videogames.map((videogame) =>
+        videogame._id === id ? res.data : videogame
+      )
+    );
+  };
 
   const deleteVideogame = async (id) => {
-    const res = await deleteVideogameRequest(id)
+    const res = await deleteVideogameRequest(id);
     if (res.status === 204) {
-      setVideogames(videogames.filter((videogame) => videogame._id !== id))
+      setVideogames(videogames.filter((videogame) => videogame._id !== id));
     }
-  }
+  };
 
   useEffect(() => {
-    getVideogames()
-  }, [])
+    getVideogames();
+  }, []);
 
-  return <videogamesContext.Provider value={{
-    videogames,
-    getVideogames,
-    getVideogame,
-    createVideogame,
-    deleteVideogame,
-    updateVideogame
-  }}>
-    {children}
-  </videogamesContext.Provider>
-}
+  return (
+    <videogamesContext.Provider
+      value={{
+        videogames,
+        getVideogames,
+        getVideogame,
+        createVideogame,
+        deleteVideogame,
+        updateVideogame,
+      }}
+    >
+      {children}
+    </videogamesContext.Provider>
+  );
+};
