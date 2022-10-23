1e0791008bde35a9db4d291a90a56524315f208f
Daniel López <hyperdaniel579exe@gmail.com>
2022-10-23 03:30:50 -0500

    App updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,34 @@
-import './App.css';
-import { HomePage, CreateVideogameForm, NotFoundPage, ViewVideogamePage, EditVideogamePage } from './pages/Index'
-import { Routes, Route, BrowserRouter } from 'react-router-dom'
-import { VideogameProvider } from './context/videogameContext'
-import { Toaster } from 'react-hot-toast'
+import "./App.css";
+import {
+  HomePage,
+  CreateVideogameForm,
+  NotFoundPage,
+  ViewVideogamePage,
+  EditVideogamePage,
+} from "./pages/Index";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { VideogameProvider } from "./context/videogameContext";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
-    <div className='parent-container'>
-      <div className='children-container'>
+    <div className="parent-container">
+      <div className="children-container">
         <VideogameProvider>
           <Toaster />
           <BrowserRouter>
             <Routes>
-              <Route path='/' element={<HomePage />} />
-              <Route path='/newVideogame' element={<CreateVideogameForm />} />
-              <Route path='/viewVideogame/:id' element={<ViewVideogamePage />} />
-              <Route path='/editVideogame/:id' element={<EditVideogamePage />} />
-              <Route path='*' element={<NotFoundPage />} />
+              <Route path="/" element={<HomePage />} />
+              <Route path="/newVideogame" element={<CreateVideogameForm />} />
+              <Route
+                path="/viewVideogame/:id"
+                element={<ViewVideogamePage />}
+              />
+              <Route
+                path="/editVideogame/:id"
+                element={<EditVideogamePage />}
+              />
+              <Route path="*" element={<NotFoundPage />} />
             </Routes>
           </BrowserRouter>
         </VideogameProvider>
